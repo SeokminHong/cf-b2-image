490ceb5e304811e996f6f2b3a52adff39124d510
Seokmin Hong <ghdtjrald240@gmail.com>
2022-04-01 17:06:57 +0900

    Add js context to route context

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@ async fn get_image<D>(
 }
 
 #[event(fetch)]
-pub async fn main(req: Request, env: Env) -> Result<Response> {
+pub async fn main(req: Request, env: Env, ctx: worker::Context) -> Result<Response> {
     log_request(&req);
 
     // Optionally, get more helpful error messages written to the console in the case of a panic.
@@ -40,7 +40,7 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
     // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
     // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
     // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
-    let router = Router::new();
+    let router = Router::with_data(ctx);
 
     // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
     // functionality and a `RouteContext` which you can use to  and get route parameters and
